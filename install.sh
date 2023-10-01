9088b699c88c38ad7c0dbd826f2a528c45b90efd
kevin Lyon <69926397+kevinLyon@users.noreply.github.com>
2023-10-01 17:56:47 -0300

    Update install.sh
    fix bugs

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
     # Download Burp Suite Profesional Latet Version
-    echo 'Downloading Burp Suite Professional Latest....'
-    html=$(curl -s https://portswigger.net/burp/releases)
-    version=$(echo $html | grep -Po '(?<=/burp/releases/professional-community-)[0-9]+\-[0-9]+\-[0-9]+' | head -n 1)
-    Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar"
-    echo $version
-    wget "$Link" -O burpsuite_pro_v$version.jar --quiet --show-progress
+    curl "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -o burpsuite_pro_latest.jar
 
     # Execute Keygenerator
     echo 'Starting Keygenerator'
@@ -15,7 +10,7 @@
     
     # Execute Burp Suite Professional Latest with Key
     echo 'Executing Burp Suite Professional Latest with Keyloader'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > Burp
-    chmod +x Burp
-    cp Burp /bin/Burp
-    (./Burp)
+    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_latest.jar &" > burpsuite_pro_latest
+    chmod +x burpsuite_pro_latest
+    cp burpsuite_pro_latest /bin/burpsuite_pro_latest
+    (./burpsuite_pro_latest)
