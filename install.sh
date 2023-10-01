d3fb669d6328e556b167cfb72dcd0e6973eea0ed
kevin Lyon <69926397+kevinLyon@users.noreply.github.com>
2023-10-01 18:12:56 -0300

    Update install.sh
    fix bugs

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+curl "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -o burpsuite_pro_latest.jar
 
-    # Download Burp Suite Profesional Latet Version
-    curl "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -o burpsuite_pro_latest.jar
+java -jar loader.jar &
+sleep 2s
 
-    # Execute Keygenerator
-    echo 'Starting Keygenerator'
-    (java -jar loader.jar) &
-    sleep 2s
-    
-    # Execute Burp Suite Professional Latest with Key
-    echo 'Executing Burp Suite Professional Latest with Keyloader'
-    echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_latest.jar &" > burpsuite_pro_latest
-    chmod +x burpsuite_pro_latest
-    cp burpsuite_pro_latest /bin/burpsuite_pro_latest
-    (./burpsuite_pro_latest)
+echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:loader.jar -noverify -jar burpsuite_pro_latest.jar &" > burpsuite_pro_latest
+chmod +x burpsuite_pro_latest
+cp burpsuite_pro_latest /bin/burpsuite_pro_latest
+./burpsuite_pro_latest
